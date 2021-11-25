412aa214bd7cb87b8b26775262aae2811d43ecc4
zeal_peace <luming12138404@gmail.com>
2021-11-25 22:31:52 +0800

    第一次尝试忽略文件提交

---
+++ helloProject/helloMoudle/src/com/zeng/hello/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.zeng.hello;
+
+/**
+ * @description:
+ * @author: Administrator
+ * @date: 2021/11/25 22:21
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("这个世界会好的！！！");
+    }
+}
