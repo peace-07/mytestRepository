6fedc2420cb47c6aab9911633344112532fd1972
zeal_peace <luming12138404@gmail.com>
2021-11-26 01:50:57 +0800

    第二次提交

--- helloProject/helloMoudle/src/com/zeng/hello/HelloWorld.java
+++ helloProject/helloMoudle/src/com/zeng/hello/HelloWorld.java
@@ -8,5 +8,7 @@ package com.zeng.hello;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("这个世界会好的！！！");
+        System.out.println("hello world！！！");
+		
     }
 }
