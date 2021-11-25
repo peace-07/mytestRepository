fd66761837c2a441a2a5c816b67aa86f528be5ab
zeal_peace <luming12138404@gmail.com>
2021-11-26 03:39:43 +0800

    Squashed commit of the following:
    commit 7f9108c686016cf5362b71a1806e26cdfefc88e1
    Author: zeal_peace <luming12138404@gmail.com>
    Date:   Fri Nov 26 03:30:27 2021 +0800
    
        repo第二次提交，修改了hello类代码

--- helloProject/helloMoudle/src/com/zeng/hello/HelloWorld.java
+++ helloProject/helloMoudle/src/com/zeng/hello/HelloWorld.java
@@ -9,6 +9,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("这个世界会好的！！！");
         System.out.println("hello world！！！");
+        System.out.println("hhhhhh");
 		
     }
 }
